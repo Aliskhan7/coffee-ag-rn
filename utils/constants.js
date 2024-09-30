6b1d623609a59d42ef9f67df2e81f01b68414b6b
aliskhangazamatov <ag@farcana.com>
2024-10-01 00:14:55 +0300

    added style, register

---
+++ utils/constants.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+    primary: '#6a1b9a',
+    secondary: '#9c27b0',
+    background: '#f3e5f5',
+    text: '#000',
+};
+
+export const FONT_SIZES = {
+    small: 12,
+    medium: 16,
+    large: 20,
+};
